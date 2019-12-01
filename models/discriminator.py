f97f3330db2f91bc33c13e2957ff468aaf04c182
Chandra Prakash Konkimalla <ee14btech11014@iith.ac.in>
2019-12-01 11:53:58 -0500

    Still Working

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,4 +1,9 @@
+from models.disc.mnist import MNIST_Linear
+from models.disc.cifar import CIFAR_Disc
 
-
-def discriminator():
-	pass
+def all_discriminator(args):
+	if args.dataset=="MNIST":
+		img_shape = 32*32*3
+		return MNIST_Linear(img_shape)
+	elif args.dataset=="CIFAR":
+		return CIFAR_Disc()
