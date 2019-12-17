a1fb8aa4dfcffd6a87222f92491d5b179327e1a7
Chandra Prakash Konkimalla <ee14btech11014@iith.ac.in>
2019-12-17 13:46:45 -0500

    Support in Eval for irevnet

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ import argparse
 import scipy.misc as scmisc
 import os
 import numpy as np
+from PIL import Image
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -12,9 +13,10 @@ def parse_args():
 	parser.add_argument('--generator_path', type=str, default="model_path")
 	parser.add_argument('--output_dir', type=str, default="images")
 	parser.add_argument('--is_realnvp', type=int, default=0)
+	parser.add_argument('--is_irevnet', type=int, default=0)
 	return parser.parse_args()
 
-def ldim(dataset, is_realnvp):
+def ldim(dataset, is_realnvp, is_irevnet):
 	if is_realnvp:
 		if dataset == "CIFAR":
 			return (3, 32, 32)
@@ -26,11 +28,22 @@ def ldim(dataset, is_realnvp):
 			return (3, 64, 64)
 		else:
 			raise
+	elif is_irevnet:
+		if dataset == "CIFAR":
+			return (8, 8, 8)
+		elif dataset == "MNIST":
+			return (4, 8, 8)
+		elif dataset == "CelebA32":
+			return (8, 8, 8)
+		elif dataset == "CelebA64":
+			return (8, 16, 16)
+		else:
+			raise
 	else:
 		return (100, 1, 1)
 
 def main(args):
-	latent_dim = ldim(args.dataset, args.is_realnvp)
+	latent_dim = ldim(args.dataset, args.is_realnvp, args.is_irevnet)
 	model = torch.load(args.generator_path)
 	model.eval()
 	print(model)
@@ -39,13 +52,29 @@ def main(args):
 	for _ in range(args.count//args.batch_size):
 		z = torch.randn(args.batch_size, *latent_dim)
 		z=z.cuda()
-		out, _ = model(z, reverse=True)
+		if args.is_irevnet:
+			if args.dataset == "MNIST":
+				_, out = model(z)
+				out = out.reshape(out.shape[0], 1, 32, 32)
+			elif args.dataset == "CIFAR":
+				_, out = model(z)
+				out = out.reshape(out.shape[0], 3, 32, 32)
+			elif args.dataset == "CelebA32":
+				_, out = model(z)
+				out = out.reshape(out.shape[0], 3, 32, 32)
+			elif args.dataset == "CelebA64":
+				_, out = model(z)
+				out = out.reshape(out.shape[0], 3, 64, 64)
+		else:
+			out, _ = model(z, reverse=True)
 		out = out.to('cpu').data.numpy()
 		out = (out+1.0)/2.0
 		out = np.clip(out,0.0,1.0)
 		for sample in out:
 			out_file = os.path.join(args.output_dir,f"{image_num}.png")
 			if sample.shape[0] == 1: sample = sample[0]
+			if args.dataset=="MNIST":
+				sample = np.array(Image.fromarray(sample).resize((28,28), resample=Image.BICUBIC))
 			scmisc.toimage(sample, cmin=0.0, cmax=1.0).save(out_file)
 			image_num += 1
 
